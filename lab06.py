677093e8fda4fe53e418e963e5b709342b8d9865
Jmcgraww <johnnymcgraw0301@gmail.com>
2023-03-09 14:21:33 -0500

    Added decode function, and if-statement to ensure password is initialized before being displayed.

--- lab06.py
+++ lab06.py
@@ -1,4 +1,4 @@
-#Made by Trey Kolos
+# Made by Trey Kolos
 def encode(password):
     encodedpassword = ''
     for element in password:
@@ -6,19 +6,37 @@ def encode(password):
         encodedpassword = encodedpassword + str(element)
     return encodedpassword
 
+
+#decode function added by John McGraw
+def decode(encodedpassword):
+    decodedpassword = ''
+    for element in encodedpassword:
+        element = abs(int(element) - 3)
+        decodedpassword = decodedpassword + str(element)
+    return decodedpassword
+
+
 def main():
     useroption = 0
     while not useroption == 3:
         print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
         useroption = int(input('Please enter an option: '))
+        encodedpassword = ''
+        decodedpassword = ''
         if useroption == 1:
-            password = str(input('Please enter your password to encode: '))
-            encodedpassword = encode(password)
+            decodedpassword = str(input('Please enter your password to encode: '))
+            encodedpassword = encode(decodedpassword)
             print("Your password has been encoded and stored!\n")
         elif useroption == 2:
-            print("The encoded password is " + encodedpassword + ", and the original password is " + password + ".")
+            if encodedpassword == '':
+                encodedpassword = str(input("Please enter your password to decode: "))
+                decodedpassword = decode(encodedpassword)
+                print(
+                    "The encoded password is " + encodedpassword + ", and the original password is " + decodedpassword + ".")
+            else:
+                print("The encoded password is " + encodedpassword + ", and the original password is " + password + ".")
         elif useroption == 3:
             break
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
